b3eb9142a60f0207d2b8e08966f7925617cef5fd
Comamanel <florent.piette9@gmail.com>
2020-03-09 14:57:20 +0100

    ajout customdate class

--- src/app/_core/_services/race.service.ts
+++ src/app/_core/_services/race.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RaceListModel } from '../_models/race-list-model';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RaceService {
-  private _raceList: BehaviorSubject<RaceListModel[]>;
+  private _raceList$: BehaviorSubject<RaceListModel[]>;
 
-  public get 
+  public get raceList(): Observable<RaceListModel[]>{
+    return this._raceList$.asObservable();
+  }
 
   constructor(
     private httpClient: HttpClient
-  ) { }
+  ) { 
+    this._raceList$ = new BehaviorSubject<RaceListModel[]>([]);
+  }
 
+  refresh(){
+    //this.httpClient.get
+  }
 
 }
